787961b1bbec69f37b035d99a0f42f20cb96dd5f
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2014-05-26 15:47:31 -0700

    Solved 071.

--- 071.py
+++ 071.py
@@ -1,5 +1,5 @@
 from fractions import Fraction
 
-rationals = set{Fraction(i*3//7,i) for i in range(1,10000) if isreduced(i*)}
-print rationals[:10]
-# print max([i for i in rationals if i ! Fraction(3/7)])
+rationals = [Fraction(i*3//7, i) for i in range(1, 1000000) if not i % 7 == 0]
+rationals = set(rationals)
+print max(rationals)
