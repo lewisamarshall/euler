62af84125dc4713703ec954a231254f0486ad65f
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2014-06-14 16:09:19 -0700

    Many changes to speed up 100, still needs work.

--- 100.py
+++ 100.py
@@ -2,17 +2,30 @@
 from math import sqrt
 import decimal
 import time
-decimal.getcontext().prec = 14
 
-n = int(10**9)
-divisor = sqrt(decimal.Decimal(2))
-print divisor
 
-start = time.time()
-while True:
-    n += 1
-    b = int(n//divisor+1)
-    if 2*b*(b-1) == n*(n-1):
-        break
-print n, int(b)
-print time.time()-start
+def prob_check(b, n):
+    if 2*(b**2-b) == (n**2-n):
+        return True
+    else:
+        return False
+
+
+def calc_blueprob():
+    decimal.getcontext().prec = 14
+
+    n = int(2*10**3)
+    divisor = sqrt(2.0)
+    print divisor
+
+    start = time.time()
+    for n in range(2*10**6, 10**7):
+        # while True:
+        # n += 1
+        b = int(n//divisor+1)
+        if prob_check(b, n):
+            break
+    print n, int(b)
+    print time.time()-start
+
+calc_blueprob()
