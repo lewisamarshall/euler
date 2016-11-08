f3053e9eb22627c48c5a2fa5f6b627ee97b0be54
Lewis A. Marshall <lewis@purigenbio.com>
2016-11-08 09:35:21 -0800

    Added code to 005 to produce a loop of factors.

--- 005/005.c
+++ 005/005.c
@@ -3,12 +3,33 @@
 #include <math.h>
 #include "../lib/factor.h"
 
+// Limit of loop
+int max = 20;
+// Record of total factors.
+int factors[20] = {0};
+
 int main(){
-  int value = 12214;
-  int *result = factor(value);
-  // print results
-  for (int i = 0; result[i]!=-1; i++) {
-    printf("%i\n", result[i]);
+
+  // Make a list of all the factors.
+  int *result;
+  for (int i = 1; i<=max; i++) {
+    printf("%i\n", i);
+    result = factor(i);
+    for (int a = 0; result[a]>0; a++) {
+      printf("%i\t", result[a]);
+      // TODO: Figure out a way of filtering here.
+      factors[result[a]]++;
+    }
   }
+
+  // Product of all factors.
+  long product = 1;
+  for (int j = 0; j < max; j++) {
+    for (int k = 0; k < factors[j]; k++) {
+      product *= j;
+    }
+  }
+  //print result
+  printf("\nProduct: %li\n", product);
   return 0;
 }
