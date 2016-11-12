6215fe51929d52efa5bff4e8b42bd8a380837b6f
Lewis A. Marshall <lewis@purigenbio.com>
2016-11-11 20:23:59 -0800

    New python and C answers to 008, drawing from txt

--- 008/008.c
+++ 008/008.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+const int n = 13;
+int i=0;
 char character=1;
+int buffer[n] = {0};
+long current = 0;
+long store = 0;
 
 int main(){
 
+  // Open the file.
   FILE *fp;
   fp = fopen ("008.txt","r");
   if (fp!=NULL) {
-    while (character!=EOF) {
-      character = fgetc(fp);
-      printf("%c", character);
+    // Iterate through the file.
+    while ((character = fgetc(fp))) {
+      if (character=='\n'){
+        break;
+      }
+      buffer[i] = atoi(&character);
+      i++; i%=n;
+      current = 1;
+      for (size_t j = 0; j < n; j++) {
+        current *= buffer[j];
+      }
+      if (current>store){
+        store=current;
+      }
     }
     fclose (fp);
+    printf("%li\n", store);
   }
   return 0;
 }
