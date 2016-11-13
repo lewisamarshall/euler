d1177d870f3f5f35c6e6a6551cfc0e00981d92a7
Lewis A. Marshall <lewis@purigenbio.com>
2016-11-12 21:27:49 -0800

    Improved readability of 001.py

--- 001/001.py
+++ 001/001.py
@@ -1,4 +1,7 @@
-total = sum(
-    [a for a in range(1, 1000)
-        if (a % 3 == 0 or a % 5 == 0)])
+total = 0
+
+for i in range(1000):
+    if i%3==0 or i%5==0:
+        total += i
+
 print(total)
