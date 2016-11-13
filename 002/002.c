f39ab2ec5290d6be1cc17bd753e0fdfbd8981745
Lewis A. Marshall <lewis@purigenbio.com>
2016-11-13 14:36:21 -0800

    Removed python references in some C files.

--- 002/002.c
+++ 002/002.c
@@ -17,14 +17,3 @@ int main(){
   printf("%i\n", total);
   return 0;
 }
-
-
-// total = 0
-// a = 1
-// b = 2
-//
-// while b < 4000000:
-//     if b % 2 == 0:
-//         total += b
-//     [a, b] = [b, a+b]
-// print(total)
--- 003/003.py
+++ 003/003.py
@@ -2,6 +2,6 @@ n = 600851475143
 i = 2
 while i*i < n:
     while n % i == 0:
-        n = n/i
+        n = n//i
     i = i+1
 print(n)
