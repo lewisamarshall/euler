178dea19b581d255d70342d23bc6ebafaeacdb06
Lewis A. Marshall <lewis@purigenbio.com>
2017-05-07 17:35:56 -0700

    Partial update to 44.

--- 044/044.py
+++ 044/044.py
@@ -1,14 +1,32 @@
-max=10000
+import itertools
 
-pentagons=[]
-sum_pentagons=[]
-n=0
-while 1:
-  current_pentagon=n*(3*n-1)/2
-  pentagons.append(current_pentagon)
-  for i in pentagons:
-    if current_pentagon-i in pentagons:
-      sum_pentagons.append(current_pentagon)
-      if i in sum_pentagons:
-        print current_pentagon, i
-        break
+computed = []
+
+def pentagonal(n):
+    p = int(n*(3*n-1)/2)
+    if p not in computed:
+        computed.append(p)
+    return p
+
+def is_pentagonal(p):
+    if p in computed:
+        return True
+
+    # n = something
+    if p>1000:
+        return True
+
+    n = 0.1
+
+    return n==int(n)
+
+for j in itertools.count(start=1, step=1):
+    pj = pentagonal(j)
+    for k in range(1, j):
+        pk = pentagonal(k)
+
+        if is_pentagonal(abs(pj-pk)):
+            if is_pentagonal(pj+pk):
+                raise StopIteration
+
+print('j: {}\nk: {}\nD: {}'.format(j, k, pj-pk))
