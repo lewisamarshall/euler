c075ae4b455155c94413628d5de2792508607fac
Lewis A. Marshall <lewis@purigenbio.com>
2022-11-05 15:54:29 -0700

    Reduce memory size by recalculating sparse high values.

--- 014/014.c
+++ 014/014.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-long  MAXSIZE = 100 000 000 000;
+long  MAXSIZE = 9223372036854775807;
 long collatz (long n);
 
-long collatz_ref[100 000 000 000];
+long collatz_ref[1000000];
 
 int main(){
     int longest = 0;
     int best_result = 0; 
     int result = 0;
-    collatz_ref[1] = 1;
 
     for (int i =1; i<=1000000; i++){
         result = collatz(i);
@@ -36,23 +35,31 @@ long collatz(long n){
     long i = 0;
     long n_orig = n; 
 
-    while(!collatz_ref[n]){
-        // printf("%li\n", n);
+    while (n>1) {
+        // printf("loop %li\n", n);
+        if (n < 100000){
+            if (collatz_ref[n]!=0){
+                // printf("lookup %li\n", n);
+                collatz_ref[n_orig] = collatz_ref[n] + i;
+                break;
+            }
+        }
+        
         i += 1;
         if (n%2==0){
+            // printf("dividing %li\n", n);
             n=n/2;
         } else {
+            // printf("multiplying %li\n", n);
             n = n*3+1;
             if (n>MAXSIZE){
-
                 return -1;
             }
         }
+    collatz_ref[n_orig] = i;
     }
 
-    collatz_ref[n_orig] = collatz_ref[n] + i;
 
-
-    return collatz_ref[n_orig]-1;
+    return collatz_ref[n_orig];
 
 }
